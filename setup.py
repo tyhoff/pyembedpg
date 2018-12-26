ba74a80fa3f60a196a02a19748348a557940446a
Tyler Hoffman <tyhoffman1@gmail.com>
2018-12-25 23:22:29 -0500

    Update version of psycopg2 to fix error
    Context:
    https://github.com/psycopg/psycopg2/issues/594

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class PyTestCommand(TestCommand):
 
 setup(
     name='pyembedpg',
-    version='0.0.3',
+    version='0.0.99',
     description='Run embedded version of Postgres',
     long_description='Run embedded version of Postgres',
     keywords='postgres, python, tests',
@@ -62,7 +62,7 @@ setup(
     ],
     install_requires=[
         'requests==2.7.0',
-        'psycopg2==2.6'
+        'psycopg2==2.7.*'
     ],
     tests_require=[
         'pytest'
